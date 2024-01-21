1d662ccc05be90a226824d72363641a6d7d80c00
Jaelson Pereira <86059311+jaelps@users.noreply.github.com>
2024-01-21 13:02:38 +0000

    retiradas imagens desnecessarias

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <>
@@ -14,7 +11,12 @@ function App() {
           <a href="">maria</a>
         </nav>
       </header>
-      <main></main>
+      <main>
+        <article>
+          <section></section>
+          <aside></aside>
+        </article>
+      </main>
       <footer></footer>
     </>
   );
